f10e036421d995e5beb5e0027f34964fed928571
Ángel Soriano <asoriano@robotnik.es>
2025-05-23 21:03:49 +0200

    re-structured files

--- kuka_gui/run_gui.sh
+++ kuka_gui/run_gui.sh
@@ -5,5 +5,5 @@ source /opt/ros/kinetic/setup.bash
 source /home/robotnik/catkin_ws/devel/setup.bash
 
 # Ejecutar la GUI desde el directorio del proyecto
-cd "$(dirname "$0")/gui/kuka_ui"
+cd "$(dirname "$0")/gui"
 python main.py
